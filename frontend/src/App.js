d1d3855641e8196a542b9e1d934587086e170eeb
amankumar739728 <124084331+amankumar739728@users.noreply.github.com>
2026-02-11 14:38:33 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { ThemeProvider } from './contexts/ThemeContext';
+import { LoadingProvider } from './contexts/LoadingContext';
 import Homepage from './components/Homepage';
 import Login from './components/Login';
 import Signup from './components/Signup';
@@ -14,76 +15,80 @@ import Rooms from './components/Rooms';
 import Payments from './components/Payments';
 import UserManagement from './components/UserManagement';
 import PrivateRoute from './components/PrivateRoute';
+import GlobalLoadingSpinner from './components/GlobalLoadingSpinner';
 
 function App() {
   return (
     <ThemeProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/verify-email" element={<VerifyEmail />} />
-          <Route
-            path="/dashboard"
-            element={
-              <PrivateRoute>
-                <Dashboard />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/rooms"
-            element={
-              <PrivateRoute>
-                <Rooms />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/payments"
-            element={
-              <PrivateRoute>
-                <Payments />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/announcements"
-            element={
-              <PrivateRoute>
-                <Announcements />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/feedbacks"
-            element={
-              <PrivateRoute>
-                <Feedbacks />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/menu"
-            element={
-              <PrivateRoute>
-                <Menu />
-              </PrivateRoute>
-            }
-          />
-          <Route path="*" element={<Navigate to="/" replace />} />
-          <Route
-            path="/user-management"
-            element={
-              <PrivateRoute>
-                <UserManagement />
-              </PrivateRoute>
-            }
-          />
-        </Routes>
-      </Router>
+      <LoadingProvider>
+        <GlobalLoadingSpinner />
+        <Router>
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/verify-email" element={<VerifyEmail />} />
+            <Route
+              path="/dashboard"
+              element={
+                <PrivateRoute>
+                  <Dashboard />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/rooms"
+              element={
+                <PrivateRoute>
+                  <Rooms />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/payments"
+              element={
+                <PrivateRoute>
+                  <Payments />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/announcements"
+              element={
+                <PrivateRoute>
+                  <Announcements />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/feedbacks"
+              element={
+                <PrivateRoute>
+                  <Feedbacks />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/menu"
+              element={
+                <PrivateRoute>
+                  <Menu />
+                </PrivateRoute>
+              }
+            />
+            <Route path="*" element={<Navigate to="/" replace />} />
+            <Route
+              path="/user-management"
+              element={
+                <PrivateRoute>
+                  <UserManagement />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </Router>
+      </LoadingProvider>
     </ThemeProvider>
   );
 }
